c342fa5471d78f60f4022078a349f0a5ea491ff1
AgabaBrian <brian.agaba02@gmail.com>
2022-07-13 10:12:15 +0300

    soln

---
+++ 0x00-hello_world/index.hello.c
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+   S
+   EMBLER
+#!/bin/bash
+gcc -S $ CFILE
+
+3-NAME
+#!/bin/bash
+
+c $CFILE -O cis fun
+
+
+/**
+ * main-entry point
+ *
+ *Return:Always o(success) 
+ */
+ int main(Void)
+ {
+     puts("/"programming is like building a multilingual puzzle"),
+     return(0)
+ }
+
+ 5- printf.c 
+ #include<stdio.h>
+ /**
+  * Main-Entry point
+  *
+  *Return :Always 0(success) 
+  */
+  int Main(Void)
+  {printf ("With proper grammer, but the outcome is a piece of Art,\n);
+  }
+
+  include <stdio.h>
+  /**
+   * Main-Entry point
+   *
+   * Return :Always 0(success)
+   */
+   int main(Void)
+   {
+       char acharacter;
+       int ainteger;
+       long along;
+       long long alonglong
+       float afloat
+       printf("size of a char:%lu bytes\n",size of (acharacter),)
+       printf ("size of a int :%lu bytes\n",size of (ainteger));
+       printf ("size of a long :%lu bytes\n",size of (along));
+       printf ("size of a long long :%lu bytes\n",size of (alonglong));
+       printf ("size of a float :% lu bytes\n",size of (afloat));
+   }
+
+
+
